b498debdd75e19d01567e557a75bbd72f35aa5f2
Maisa Ahmad <2013maisaah@gmail.com>
2020-04-18 10:26:39 -0400

    added responsive poem

---
+++ week 09/script.js
@@ -0,0 +1,7 @@
+document.getElementById("date").innerHTML = new Date().toDateString();
+
+function myFunction() {
+    var Change = document.getElementById("change");
+    Change.style.color = "red"; 
+
+  }
---
+++ week 11/script.js
@@ -0,0 +1,12 @@
+document.getElementById("date").innerHTML = new Date().toDateString();
+
+function myFunction() {
+    var Change = document.getElementById("change");
+    Change.style.color = "red"; 
+
+  }
+
+
+  // $("#date").on("click", function() {
+  //   $(".container").removeClass("")
+  // })
